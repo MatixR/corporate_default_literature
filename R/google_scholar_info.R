d9ea993504e4f3a1f6b5f39e179cd53e0e875c42
boennecd <boennecd@gmail.com>
2017-01-11 17:27:18 +0100

    Got blocked by google so I will call it a day with the cooding

---
+++ R/google_scholar_info.R
@@ -0,0 +1,63 @@
+get_google_scholar_info <- with(new.env(), {
+  require(stringr)
+  require(rvest)
+  
+  last_call <- Sys.time()
+  
+  function(name = "", journal = ""){
+    # Try to open the url
+    url_ <- paste0("https://scholar.google.ch/scholar?as_q=&as_epq=", 
+                   gsub("\\ ", "+", name), "&as_publication=", 
+                   gsub("\\ ", "+", journal), "&hl=en")
+    
+    # Dont want issues with google searches blooking us out
+    # cat("\nBefore:", 1 - as.numeric(Sys.time()) + as.numeric(last_call))
+    Sys.sleep(max(1e-8, 1 - as.numeric(Sys.time()) + as.numeric(last_call)))
+    last_call <<- Sys.time()
+    # cat("\nAfter: ", 1 - as.numeric(Sys.time()) + as.numeric(last_call))
+    
+    did_fail <- FALSE
+    tryCatch({
+      site <- read_html(url_)
+    }, error = function(e){
+      warning("Failed with url ", url_, ". Errors is", "\n")
+      warning(str(e))
+      did_fail <<- TRUE
+    })
+    
+    if(did_fail){
+      return(invisible())
+    }
+    
+    # Extract the html nodes with the results
+    res_node <- html_nodes(
+      site, xpath = "//div[@id = 'gs_ccl_results']/div/div[contains(@class, 'gs_ri')]")
+    
+    # Only return something if we found exactly one result
+    if(length(res_node) > 1){
+      warning("Found multiple matches for ", name, " in ", journal, "\n")
+      return(invisible())
+    } else if(length(res_node) == 0){
+      warning("Did not find any matches for ", name, " in ", journal, "\n")
+      return(invisible())
+    }
+    
+    # Get the journal url and number of citations and return
+    journal_url <- html_text(html_nodes(
+      res_node, xpath = "./h3[contains(@class, 'gs_rt')]/a/@href"))
+    
+    n_citations <- html_text(html_nodes(
+      res_node, xpath = "(./div[contains(@class, 'gs_fl')]/a)[1]/text()"))
+    n_citations <-  as.numeric(gsub("^Cited by ", "", n_citations))
+    
+    year <- html_text(html_nodes(
+      res_node, xpath = "./div[contains(@class, 'gs_a')]/text()"))
+    year <- as.numeric(str_extract(tail(year, 1), "\\d{4}"))
+    
+    list(journal_url = journal_url, year = year, n_citations = n_citations)
+  }
+})
+
+# get_google_scholar_info("Design and natural science research on information technology",
+#                         "Decision support systems")
+            
