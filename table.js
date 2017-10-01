3cc69c3607835f9a0f82db470ef5cb58a3d4c47f
Ben <boennecd@gmail.com>
2017-10-01 10:15:44 +0200

    Added .bib file to html

--- table.js
+++ table.js
@@ -1,5 +1,6 @@
 <!--html_preserve-->
 <script src='js/jquery.dataTables.min.js'></script>
+<script src='js/dataTables.fixedColumns.min.js'></script>
 <script type="text/javascript">
 // Sort function for NA from https://datatables.net/forums/discussion/7446/numeric-sort-with-na-in-some-cells
 $.fn.dataTableExt.oSort['numeric_ignore_nan-asc'] = function(x,y) {
@@ -20,23 +21,27 @@ $.fn.dataTableExt.oSort['numeric_ignore_nan-desc'] = function(x,y) {
 };
 $(document).ready(function() {
 	$('#review_tbl').DataTable( {
-		"scrollY": "600px",
+		"scrollY": "800px",
 		"scrollX": true,
+		"pageLength": 4,
+		"lengthMenu": [4, 8, 12],
 		"scrollCollapse": true,
+		"scrollCollapse": true,
+		fixedColumns: { leftColumns: 2 },
 		"order": [[5, 'asc'], [1, 'desc']],
 		"columnDefs": [
-			{ "targets": 0, "className": "dt-body-left", "width": "15em" }, 
+			{ "targets": 0, "className": "dt-body-left", "width": "15em", "searchable": true, }, 
 			{ "targets": 1, "className": "dt-body-right", "width": "5em", "type": "numeric_ignore_nan" }, 
-			{ "targets": 2, "className": "dt-body-left", "width": "18em" }, 
-			{ "targets": 3, "className": "dt-body-left", "width": "10em" }, 
-			{ "targets": 4, "className": "dt-body-left", "width": "20em" }, 
+			{ "targets": 2, "className": "dt-body-left", "width": "18em", "searchable": true, }, 
+			{ "targets": 3, "className": "dt-body-left", "width": "10em", "searchable": true, }, 
+			{ "targets": 4, "className": "dt-body-left", "width": "20em", "searchable": true, }, 
 			{ "targets": 5, "className": "dt-body-center", "width": "9em", "type": "numeric_ignore_nan" }, 
 			{ "targets": 6, "className": "dt-body-left", "width": "40em" }, 
 			{ "targets": 7, "className": "dt-body-center", "width": "5em" }, 
-			{ "targets": 8, "className": "dt-body-left", "width": "40em" }, 
-			{ "targets": 9, "className": "dt-body-left", "width": "20em" }, 
-			{ "targets": 10, "className": "dt-body-left", "width": "20em" }, 
-			{ "targets": 11, "className": "dt-body-left", "width": "20em" }, 
+			{ "targets": 8, "className": "dt-body-left", "width": "40em", "searchable": true, }, 
+			{ "targets": 9, "className": "dt-body-left", "width": "20em", "searchable": true, }, 
+			{ "targets": 10, "className": "dt-body-left", "width": "20em", "searchable": true, }, 
+			{ "targets": 11, "className": "dt-body-left", "width": "20em", "searchable": true }, 
 			{ "targets": 12, "className": "dt-body-center", "width": "5em" }, 
 			{ "targets": 13, "className": "dt-body-left", "width": "10em" }, 
 			{ "targets": 14, "className": "dt-body-left", "width": "1em" }
